8444f34f47fc2eb7464470523f4ee5eb6227c136
Akbar cp <akbar@eduport.app>
2024-02-17 21:34:55 +0530

    asset added

---
+++ lib/src/helpers/images.dart
@@ -0,0 +1,10 @@
+class PlayerImages {
+  static const exitFullScreen = 'assets/player/exit_full_screen.svg';
+  static const fullScreen = 'assets/player/full_screen.svg';
+  static const pause = 'assets/player/pause.svg';
+  static const play = 'assets/player/play.svg';
+  static const quality = 'assets/player/quality.svg';
+  static const seekBack = 'assets/player/seek_back.svg';
+  static const seekFront = 'assets/player/seek_front.svg';
+  static const speed = 'assets/player/speed.svg';
+}
